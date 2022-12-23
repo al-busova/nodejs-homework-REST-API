77765698e248cd92169ce03d7189c957e8df855a
Alena Busova <96522313+al-busova@users.noreply.github.com>
2022-12-23 18:06:28 +0200

    Update addContact.js

--- controllers/contacts/addContact.js
+++ controllers/contacts/addContact.js
@@ -2,9 +2,6 @@ const { Contact } = require("../../models/contact");
 
 const addContact = async (req, res, next) => {
   try {
-    if (!req.body.favorite) {
-      req.body.favorite = false;
-    }
     const newContact = await Contact.create(req.body);
     res.status(201).json({
       status: "success",
